9869d861eb4a2c724e5feffb7499eab614d2aae1
bharathikalai <108749744+bharathikalai@users.noreply.github.com>
2022-07-12 12:29:00 +0530

    msg

---
+++ jenkins.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+ public static void main(String[] args) {
+  // TODO AUTO-generated method stub
+    System.out.println("Hello Agni");
+System.out.println("Hello Riya");
+}
+}
