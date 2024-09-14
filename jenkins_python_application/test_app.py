1d91052a694b38a483dbed07f2a67a5a596691b9
nila <bharathikalai.bk@gmail.com>
2024-09-14 21:00:01 +0530

    new

---
+++ jenkins_python_application/test_app.py
@@ -0,0 +1,17 @@
+import unittest
+from app import app
+
+class TestFlaskApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_hello_world(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), 'Hello, World!')
+
+if __name__ == '__main__':
+    unittest.main()
+
